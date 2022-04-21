cdb1031ba97acbd6720c8c117fc6bce031a10534
serge <agentlukas321@gmail.com>
2022-04-21 18:31:22 +0300

    Created class SalesView: add class model and constructor

---
+++ src/view/SalesView.java
@@ -0,0 +1,17 @@
+package view;
+
+import model.Product;
+
+public class SalesView {
+    Product model;
+    String title;
+    String name;
+    int price;
+    double weight;
+
+    public SalesView(Product model) {
+        this.model = model;
+    }
+
+
+}
