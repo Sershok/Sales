e9238fea4d2efc5ea81fa244ebbe9270e988f914
serge <agentlukas321@gmail.com>
2022-04-21 18:44:40 +0300

    Update class SalesView: add method getInputs

--- src/utils/Validator.java
+++ src/utils/Validator.java
@@ -15,34 +15,43 @@ public class Validator {
         return str;
     }
 
+    public static String validatePhone(Scanner scanner){
+        String str = scanner.nextLine().trim();
+        while (str.isEmpty()) {
+            System.out.println("Пусто! Введите номер клиента: ");
+            str = scanner.nextLine().trim();
+        }
+        return str;
+    }
+
     // Валидация ввода количества
-    public static int validateQuantityInput(Scanner scanner){
-        while (!scanner.hasNextInt()) {
+    public static double validateQuantityInput(Scanner scanner){
+        while (!scanner.hasNextDouble()) {
             String str = scanner.nextLine().trim();
             System.out.printf("\"%s\" - не число!\n", str);
             System.out.println("Введите количество!: ");
         }
-        int quantity = scanner.nextInt();
+        double quantity = scanner.nextDouble();
         while (quantity <= 0){
             System.out.println("Неверное значение! Введите количество: ");
-            while (!scanner.hasNextInt()) {
+            while (!scanner.hasNextDouble()) {
                 String str = scanner.next().trim();
                 System.out.printf("\"%s\" - не число!\n", str);
                 System.out.println("Введите количество!: ");
             }
-            quantity = scanner.nextInt();
+            quantity = scanner.nextDouble();
         }
         return quantity;
     }
 
     // Валидация ввода цены
-    public static double validatePriceInput(Scanner scanner){
-        while (!scanner.hasNextDouble()) {
+    public static int validatePriceInput(Scanner scanner){
+        while (!scanner.hasNextInt()) {
             String str = scanner.nextLine().trim();
             System.out.printf("\"%s\" - не число!\n", str);
             System.out.println("Введите цену!: ");
         }
-        double price = scanner.nextDouble();
+        int price = scanner.nextInt();
         while (price <= 0) {
             System.out.println("Неверное значение! Введите цену: ");
             while (!scanner.hasNextInt()) {
@@ -50,7 +59,7 @@ public class Validator {
                 System.out.printf("\"%s\" - не число!\n", str);
                 System.out.println("Введите количество!: ");
             }
-            price = scanner.nextDouble();
+            price = scanner.nextInt();
         }
         return price;
     }
--- src/view/SalesView.java
+++ src/view/SalesView.java
@@ -1,11 +1,16 @@
 package view;
 
 import model.Product;
+import utils.Validator;
+
+import java.util.Scanner;
 
 public class SalesView {
+    Scanner sc;
     Product model;
     String title;
-    String name;
+    String nameProduct;
+    String phone;
     int price;
     double weight;
 
@@ -13,5 +18,30 @@ public class SalesView {
         this.model = model;
     }
 
+    public void getInputs() {
+
+        sc = new Scanner(System.in);
+
+        title = "Product name";
+        System.out.println(title);
+        nameProduct = Validator.validateName(sc);
+        model.setNameProduct(nameProduct);
+
+        title = "Phone client";
+        System.out.println(title);
+        phone = Validator.validatePhone(sc);
+        model.setPhone(phone);
+
+        title = "Enter price";
+        System.out.println(title);
+        price=Validator.validatePriceInput(sc);
+        model.setPrice(price);
+
+        title = "Enter weight";
+        System.out.println(title);
+        weight=Validator.validateQuantityInput(sc);
+        model.setWeight(weight);
+
+    }
 
 }
