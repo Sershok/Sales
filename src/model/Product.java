35fe0213fc9c76d74ded789b105f91e8aa1f3177
serge <agentlukas321@gmail.com>
2022-04-21 17:27:52 +0300

    Update class Product: created vars and created getter, setter

--- src/model/Product.java
+++ src/model/Product.java
@@ -5,12 +5,38 @@ import base.Income;
 
 public class Product implements Income, IFiscal {
 
+    private String nameProduct;
+    private int price;
+    private double weight;
+    private final double BONUS = 0.15;
 
+    public String getNameProduct() {
+        return nameProduct;
+    }
 
+    public void setNameProduct(String nameProduct) {
+        this.nameProduct = nameProduct;
+    }
 
+    public int getPrice() {
+        return price;
+    }
 
+    public void setPrice(int price) {
+        this.price = price;
+    }
 
+    public double getWeight() {
+        return weight;
+    }
 
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getBONUS() {
+        return BONUS;
+    }
 
     @Override
     public double bonus(double payment) {
