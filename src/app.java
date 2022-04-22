2884eea026adcf085fffbc569002a49ed7e82191
serge <agentlukas321@gmail.com>
2022-04-22 16:42:55 +0300

    Add class app

--- src/app.java
+++ src/app.java
@@ -1,10 +1,15 @@
-public class app {
 
 /*
-*@author: Ponomarenko Sergey
-*@version 1.0.0
-*/
+ *@author: Ponomarenko Sergey
+ *@version 1.0.0
+ */
 
+import model.Product;
+import controllers.ProductController;
+import view.SalesView;
+
+
+public class app {
 
     public static void main(String[] args) {
 
