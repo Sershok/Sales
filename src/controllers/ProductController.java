7c625a540a8f9b320b9e667f7188be79b6e3b46e
serge <agentlukas321@gmail.com>
2022-04-22 16:29:35 +0300

    Update class ProductController: created method runApp

--- src/controllers/ProductController.java
+++ src/controllers/ProductController.java
@@ -15,5 +15,27 @@ public class ProductController {
         this.view = view;
     }
 
+    public void runApp(){
+
+        view.getInputs();
+
+        String nameProduct = model.getNameProduct();
+        String phone = model.getPhone();
+
+        double income = model.calculate(model.getPrice(), model.getWeight());
+        String incomeRounder = Rounder.roundValue(income);
+
+        double bonus = model.bonus(income);
+        String bonusRounder = Rounder.roundValue(bonus);
+
+        double totalIncome = model.calculate(income,bonus);
+        String totalIncomeRounder = Rounder.roundValue(totalIncome);
+
+        String output = "Product name: " + nameProduct +
+        "\nPhone: " + phone + "\nSum: " + incomeRounder
+                + "\nBonus: " + bonusRounder + "\nTo pay:"
+                + totalIncomeRounder + "\nPhone bonus:" + bonusRounder;
+
+    }
 
 }
